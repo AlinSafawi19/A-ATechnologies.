6f4c7b2b4fc761fb06bed4351f6b36fa72bff979
AlinSafawi19 <alinsafawi19@gmail.com>
2025-05-14 05:32:03 +0300

    scroll

--- script.js
+++ script.js
@@ -39,7 +39,9 @@ document.addEventListener('scroll', function () {
   var scrollPosition = window.scrollY;
   var parallax = document.querySelector('.parallax-about');
 
-  parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0.5) + 'px';
+  if (parallax) {  // Check if the element exists
+    parallax.style.backgroundPosition = 'center ' + (scrollPosition * 0.5) + 'px';
+  }
 });
 
 document.getElementById("contact-form").addEventListener("submit", function (event) {
